cc603947afdd768594aa054c7e2dd4562461c1d1
Alex Otero <fos.alex@gmail.com>
2014-05-10 20:01:04 -0300

    Se hacen pruebas con un canvas en ionic

--- pruebas/ionic/EPA/www/js/controllers.js
+++ pruebas/ionic/EPA/www/js/controllers.js
@@ -5,15 +5,34 @@ angular.module('starter.controllers', [])
 
 .controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 },
-    { title: 'Rock', id: 7 }
+      {title: 'Products', id: 1 },
+      {title: 'Map', id: 2 }
   ];
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
+    $scope.squares = [
+        {x : 0, y: 0, height: 20, length: 50},
+        {x : 0, y: 0, height: 20, length: 50},
+        {x : 0, y: 0, height: 50, length: 50}
+    ];
+})
+
+
+.directive('ngRaphaelSquare', function(){
+        return {
+            link: function(scope, element, attrs) {
+                debugger;
+                var r = Raphael(element[0]);
+
+                r.add([
+                    {type:"rect", x:0, y:0,   width:100, height:60},
+                    {type:"rect", x:100, y:0, width:100, height:60},
+                    {type:"rect", x:200, y:0, width:100, height:60},
+                    {type:"rect", x:400, y:0, width:100, height:60},
+                    {type:"rect", x:500, y:130, width:100, height:60}
+
+                ]);
+            }
+        };
 })
