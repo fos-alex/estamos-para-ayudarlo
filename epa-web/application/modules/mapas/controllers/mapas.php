88c3b88a0b37f5e01d247a75c44e449b76bf7ff4
pdbenitez <pdanibenitez91@gmail.com>
2014-08-24 19:28:23 -0300

    Servicios para guardar y obtener mapas de una sucursal

--- epa-web/application/modules/mapas/controllers/mapas.php
+++ epa-web/application/modules/mapas/controllers/mapas.php
@@ -1,57 +1,100 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+if (! defined ( 'BASEPATH' ))
+	exit ( 'No direct script access allowed' );
 
 /**
  * mapas controller
  */
-class mapas extends Front_Controller
-{
-
-	//--------------------------------------------------------------------
-
-
+class mapas extends Api_Controller {
+	
+	// --------------------------------------------------------------------
+	
 	/**
 	 * Constructor
 	 *
 	 * @return void
 	 */
-	public function __construct()
-	{
-		parent::__construct();
-
-		$this->load->library('form_validation');
-		Assets::add_module_js('mapas', 'mapas.js');
-
-	}
-
-	//--------------------------------------------------------------------
-
-
-	/**
-	 * Displays a list of form data.
-	 *
-	 * @return void
-	 */
-	public function index()
-	{
-		Template::render();
-	}
-
-	/**
-	 * Displays a list of form data.
-	 *
-	 * @return void
-	 */
-	public function editar()
-	{
-		//PODES AGREGAR JS en la carpeta assets de este modulo
-		//Assets::add_module_js('mapas', 'mapas.js'); 
-		//O ASI de cualquier lado
-		//Assets::add_js("//maps.googleapis.com/maps/api/js?key=AIzaSyBnU9uPcmEmXTau_3noivK_G8Z17MjhpAo&sensor=false&libraries=places&r");
+	public function __construct() {
+		parent::__construct ();
 		
-		Template::render();
+		// $this->load->library('form_validation');
+		// Assets::add_module_js('mapas', 'mapas.js');
 	}
-	//--------------------------------------------------------------------
+	
+	// --------------------------------------------------------------------
+	
+	/**
+	 * Displays a list of form data.
+	 *
+	 * @return void
+	 */
+	public function editar() {
+		// PODES AGREGAR JS en la carpeta assets de este modulo
+		// Assets::add_module_js('mapas', 'mapas.js');
+		// O ASI de cualquier lado
+		// Assets::add_js("//maps.googleapis.com/maps/api/js?key=AIzaSyBnU9uPcmEmXTau_3noivK_G8Z17MjhpAo&sensor=false&libraries=places&r");
+		Template::render ();
+	}
+	// --------------------------------------------------------------------
+	
+	
+	public function GET() {
+		if (array_key_exists ( 0, $this->PARAMETROS )) {
+			$id_sucursal = $this->PARAMETROS [0];
+			$this->JSON_OUT->data = $this->obtenerMapa ( $id_sucursal );
+		} else {
+			$this->JSON_OUT->data = $this->obtenerMapas ();
+		}
+	}
+	
+	public function PUT(){
+		if(array_key_exists(0, $this->PARAMETROS)){
+			$this->JSON_OUT->data = $this->modificarMapa($this->PARAMETROS[0],$this->JSON_IN);
+		}else{
+			$this->error(405,"Falta parametro");
+		}
+	
+	}
+	
+	
+	private function obtenerMapa($id) {
+		$this->load->model ( 'mapas_model', null, true );
+		$sucursal = $this->mapas_model->find ( $id );
+	
+		if (! sucursal) {
+			$this->error ( 404, "El mapa $id no existe" );
+		}
+		return $sucursal->mapa;
+	}
+	
+	private function obtenerMapas(){
+		$this->load->model('mapas_model', null, true);
+	
+		$sucursales = $this->mapas_model->find_all();
+		$mapas = array();
+		
+		foreach ($sucursales as $key => $unaSucursal) {
+			array_push($mapas, array("mapa"=> $this->obtenerMapa($unaSucursal->id)));
+		}
+		return $mapas;
+	}
+	
+	private function modificarMapa($id,$mapa){
+				
+		$this->load->model ( 'mapas_model', null, true );
+		$sucursal = $this->mapas_model->find ( $id );
+		$sucursal['mapa'] = $mapa['mapa'];
+		
+		if($this->mapas_model->update($id,$sucursal)){
+			return array("id"=>$id);
+		}else{
+			$this->error(406,"Error modificando el mapa $id");
+		}	
+	}
 
-
-
+	
+	
+	
+	
+	
 }
