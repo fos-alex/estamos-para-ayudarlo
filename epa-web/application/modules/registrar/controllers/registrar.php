b8b1c2c93ec76afe66225250962ccf03239c432e
pdbenitez <pdanibenitez91@gmail.com>
2014-08-29 01:26:35 -0300

    Se agregan validaciones y msj de errores

--- epa-web/application/modules/registrar/controllers/registrar.php
+++ epa-web/application/modules/registrar/controllers/registrar.php
@@ -1,72 +1,78 @@
-<?php if (!defined('BASEPATH')) exit ('No direct script access allowed');
+<?php if (! defined ( 'BASEPATH' ))	exit ( 'No direct script access allowed' );
 
-class Registrar extends Api_Controller
-{
-
-	
-	protected $JSON_OUT;
-	
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    public function POST()
-    {
-        $this->load->library('users/auth');
-        $this->load->model('users/user_model');
-
-        $data ['username'] = $this->JSON_IN['email'];
-        $data ['email'] = $this->JSON_IN['email'];
-        $data ['password'] = $this->JSON_IN['password'];
-        $data ['active'] = 1;
-
-        // Cargo validaciones
-        $_POST = $data;
-        $this->load->library('form_validation');
-        $this->form_validation->set_rules('email', 'lang:bf_email', 'required|trim|valid_email|max_length[50]|unique[users.email]');
-        $this->form_validation->set_rules('password', 'lang:bf_password', 'required|max_length[40]|valid_password');
-
-        if ($this->form_validation->run() !== FALSE) {
-
-            if ($user_id = $this->user_model->insert($data)) {
-            	
-            	$data_mail =  array(
-		            'to' => $data['email'],
-		            'subject' => str_replace('[SITE_TITLE]', $this->settings_lib->item('site.title'), lang('us_account_reg_complete')),
-		            'message' => lang('us_account_active_login'). lang('us_account_active_login_fin'). lang('us_epa_staff')
-		        );
-            	
-                $this->sendMail($data_mail);
-                
-                header('HTTP/1.1 200 Usuario creado correctamente');
-                $this->JSON_OUT->data = (array("id" => $user_id));
-                $this->success();
-            } else {
-                $this->error(1000, 'us_registration_fail');
-            }
-
-        } else {
-            $this->error(455, $this->form_validation->validation_errors_list());
-            
-        }
-    }
-
-
-    private function sendMail($mail)
-    {
-
-        $this->load->library('email');
-        $this->load->library('emailer/emailer');
-        $this->load->model('emailer/emailer_model');
-
-        if (!$this->emailer->send($mail)) {
-            $error = true;
-        }
-
-        if ($error) {
-            $this->error(450, "Error al enviar email");
-        }
-    }
-
+class Registrar extends Api_Controller {
+	public function __construct() {
+		parent::__construct ();
+	}
+	public function POST() {
+		$this->load->library ( 'users/auth' );
+		$this->load->model ( 'users/user_model' );
+		
+		$usuario_registro = $this->JSON_IN;
+		$data ['username'] = $usuario_registro ['email'];
+		$data ['email'] = $usuario_registro ['email'];
+		$data ['password'] = $usuario_registro ['password'];
+		$data ['active'] = 1;
+		
+		$this->controlarValidaciones ( $data );
+		
+		if ($user_id = $this->user_model->insert ( $data )) {
+			
+			$user_datos = "NOMBRE DE USUARIO: $data ['username']<br/>CONTRASEÑA: $data ['password']<br/>";
+			
+			$data_mail = array (
+					'to' => $data ['email'],
+					'subject' => str_replace ( '[SITE_TITLE]', $this->settings_lib->item ( 'site.title' ), lang ( 'us_account_reg_complete' ) ),
+					'message' => lang ( 'us_account_active_login' ) .$user_datos . lang ( 'us_account_active_login_fin' ) . lang ( 'us_epa_staff' ) 
+			);
+			$this->sendMail ( $data_mail );
+			
+			header ( 'HTTP/1.1 200 Usuario creado correctamente' );
+			$this->JSON_OUT->data = (array ("id" => $user_id));
+			$this->success();
+		} else {
+			$this->error ( 409, "Ha ocurrido un error al registrar usuario. Intente nuevamente más tarde" );
+		}
+	}
+	private function controlarValidaciones($data) {
+		if (! $data || ! array_key_exists ( 'email', $data ) || ! array_key_exists ( 'password', $data )) {
+			$this->error ( 405, "Debe ingresar datos obligatorios" );
+		}
+		
+		$_POST = $data;
+		$this->load->library ( 'form_validation' );
+		
+		$this->form_validation->set_rules ( 'email', 'lang:bf_email', 'trim|valid_email' );
+		if (! ($this->form_validation->run () !== FALSE)) {
+			$this->error ( 406, "El formato de correo electronico ingresado es incorrecto" );
+		}
+		
+		$this->form_validation->set_rules ( 'email', 'lang:bf_email', 'unique[users.email]' );
+		if (! ($this->form_validation->run () !== FALSE)) {
+			$this->error ( 407, "Ya existe un usuario con ese correo electronico" );
+		}
+		
+		if (! $this->password_correcta ( $data ['password'] )) {
+			$this->error ( 408, "Formato de clave incorrecto. Debe ser alfanumerica entre 6 y 40 caracteres y contener al menos 1 letra y 1 numero" );
+		}
+	}
+	private function sendMail($mail) {
+		$this->load->library ( 'email' );
+		$this->load->library ( 'emailer/emailer' );
+		$this->load->model ( 'emailer/emailer_model' );
+		
+		if (! $this->emailer->send ( $mail )) {
+			$error = true;
+		}
+		
+		if ($error) {
+			$this->error ( 450, "Error al enviar email" );
+		}
+	}
+	public function password_correcta($str) {
+		if (0 === preg_match ( '/[0-9]/', $str ) || 0 === preg_match ( '/[a-z]/', $str )) {
+			return FALSE;
+		}
+		return TRUE;
+	}
 }
