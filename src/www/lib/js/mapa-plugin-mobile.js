8b12d0b9f02dad1bb0fb68cb19fb7c2d40f7b3f6
Santiago Otero <santiago.otero@intraway.com>
2014-09-11 18:15:38 -0300

    is point in polygon

--- src/www/lib/js/mapa-plugin-mobile.js
+++ src/www/lib/js/mapa-plugin-mobile.js
@@ -180,7 +180,15 @@
         canvas.stage.draw();
     };
 
+/*    $.fn.isPointInside
 
+    function isPointInPoly(poly, pt){
+        for(var c = false, i = -1, l = poly.length, j = l - 1; ++i < l; j = i)
+            ((poly[i].y <= pt.y && pt.y < poly[j].y) || (poly[j].y <= pt.y && pt.y < poly[i].y))
+                && (pt.x < (poly[j].x - poly[i].x) * (pt.y - poly[i].y) / (poly[j].y - poly[i].y) + poly[i].x)
+            && (c = !c);
+        return c;
+    }*/
     $.fn.canvasMap.positionUser = function(position) {
         this.removeShape('.user');
         var user = this.getUserShape(position);
