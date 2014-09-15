1c5851d98caf523d4e1948b24c1a7111291389d6
pvillamil25 <pvillamil25@hotmail.com>
2014-09-14 22:18:27 -0300

    Subo promociones

--- src/www/js/controllers/main-menu-controller.js
+++ src/www/js/controllers/main-menu-controller.js
@@ -1,26 +1,36 @@
 angular.module('EPA.controllers')
 
-.controller('MainMenuCtrl', ['$scope','$ionicPopup','$state', function($scope, $ionicPopup, $state) {
-  $scope.menuItems = [
-      {title: 'Scanner',         id: 1, href: "barcodescanner"  },
-      {title: 'Map',             id: 2, href: "map" },
-      {title: 'NFC Reader',      id: 3, href: "nfcreader" },
-      {title: 'Geolocalization', id: 4, href: "geo" },
-      {title: 'ABM Listas',      id: 5, href: "listas" }
-  ];
-        /*Promociones.get($stateParams.idPromociones, {}).then(function(response){
-                $scope.promociones = response;
-            }
-        );*/
+    .controller('MainMenuCtrl', ['$scope','$ionicPopup','$state','Promociones',
+        function($scope, $ionicPopup, $state,Promociones) {
+            $scope.menuItems = [
+                {title: 'Scanner',         id: 1, href: "barcodescanner"  },
+                {title: 'Map',             id: 2, href: "map" },
+                {title: 'NFC Reader',      id: 3, href: "nfcreader" },
+                {title: 'Geolocalization', id: 4, href: "geo" },
+                {title: 'ABM Listas',      id: 5, href: "listas" }
+            ];
 
-        $scope.showAlert = function() {
-            $ionicPopup.alert({
-                title: 'Promociones Vigentes',
-                template: 'Las promociones vigentes son:'
-            }).then(function(res) {
-                $state.go('app.map');
-            });
 
-        };
+            $scope.showAlert = function() {
+                $scope.promociones=null;
+                Promociones.get().then(
 
-}])
+                    function(response) {
+                        $scope.promociones = response;
+                        $ionicPopup.alert({
+                            templateUrl: '/#/app/promociones.html',
+                            title: 'hola'
+                            //template: 'Las promociones vigentes son:' + JSON.stringify(response)
+
+                        }).then(function() {
+                            $state.go('app.map');
+                        });
+
+
+
+                    });
+
+
+            };
+
+        }])
---
+++ src/www/js/services/promociones-service.js
@@ -0,0 +1,18 @@
+angular.module('EPA.services')
+
+    .factory('Promociones',
+        ['$http', 'CONFIG', 'Resource',
+            function promocionesFactory ($http, CONFIG, Resource)
+            {
+                var key = 'promociones';
+
+                Resource.init(key);
+
+                return {
+                    get : function () {
+                        return Resource.get(undefined, key, {});
+                    }
+
+                };
+            }]);
+
