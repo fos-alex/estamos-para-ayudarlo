7c0b3c2fccd02288193778db94666528ada06ff3
Santiago Otero <santiago.otero@intraway.com>
2014-09-15 20:03:46 -0300

    Hardcodeo producto al scanner qr si no esta en el celular

--- src/www/js/controllers/consultar-controller.js
+++ src/www/js/controllers/consultar-controller.js
@@ -1,17 +1,19 @@
 angular.module('EPA.controllers')
 
-.controller('ConsultarCtrl', ['$scope', '$stateParams', 'Producto', 'QRReader',
+.controller('ConsultarCtrl', [
+            '$scope', '$stateParams', '$location', 'QRReader',
     function($scope, $stateParams, $location, QRReader) {
 
-    //RESPONSE HARDCODEADO PARA PROBAR, TO-DO: TEMA QR
-    var response = {"codigo":0,"mensaje":"Operacion Satisfactoria","data":[{"id":"1","nombre":"Coca","descripcion":"Gaseosa de primera linea","precio":"20","imagen":"./img/coke.jpg","info":"http://www.coca-cola.com.ar/"}]};
-    $scope.producto = response.data[0];
+    $scope.scan = function () {
+        QRReader.read(function (err, response) {
+            $scope.producto = response[0];
+            return $location.path('/app/consultar/' + $scope.producto.id);
+        });
+    };
 
-//        $scope.producto = QRReader.read();
-//        var path = '/app/consultar/';
-//        return $location.path(path+ $scope.producto.id);   
-        
+//
+
 //        Producto.get($stateParams.idProducto, {}).then(function(response) {
 //            $scope.producto = response;
 //        });
-}])
+}]);
