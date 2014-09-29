1494d58ce9aca67dc2e688df89705cc2daad5e7f
Pablo Mussari <pablomussari@gmail.com>
2014-09-28 23:05:29 -0300

    Facebook Login

--- src/www/js/directives/fb-login-directive.js
+++ src/www/js/directives/fb-login-directive.js
@@ -5,38 +5,56 @@ angular.module('EPA.directives', [
 secret : 42d757114f3ae3d6dd4bd0b68fcf335a
 */
 
-.directive('fbLogin', ['$window',function($window){
+.directive('fbLogin', ['$window', '$state','User',function($window,$state,User){
+
+    var getFacebookData = function(success,error){
+        facebookConnectPlugin.api( "me/?scope=email", ["email"],success,error);
+    };
+
+    var checkRegister = function(){
+        getFacebookData(function(userData){
+            User.loginUserFacebook(userData.id).then(function(response){
+                    $state.go('app.menu');    
+                },function(failure){
+                    User.registerUserFacebook(userData).then(function(response){
+                        User.loginUserFacebook(userData.id).then(function(response){
+                            $state.go('app.menu');    
+                        });
+                    },function(error){
+                        console.log("ERROR REGISTER FB USER "+ JSON.stringify(error));
+                    });
+                }
+            );
+        },function(response){
+            console.log("DATA ERROR "+JSON.stringify(response));
+        });
+    };
+
     return {
         restrict: 'A',
         link: function(scope, element, attrs) { 
+            if (!window.cordova) {
+                var appId = 1504374299806491;
+                facebookConnectPlugin.browserInit(appId);
+            }
+
           element.find("a").on('click',function(){
-              FB.login(function(response) {
-                 if (response.authResponse) {
-                   console.log('Usuario logueado');
-                   FB.api('/me', function(response) {
-                     console.log('Nombre : ' + response.name );
-                   });
-                 } else {
-                   console.log('User cancelled login or did not fully authorize.');
-                 }
-              });
-          });
-
-          window.fbAsyncInit = function() {
-            FB.init({
-              appId      : '1504374299806491',
-              xfbml      : true,
-              version    : 'v2.0'
+            facebookConnectPlugin.getLoginStatus(function(response){
+                if(response.status == 'connected'){
+                        checkRegister();
+                }else{
+                  facebookConnectPlugin.login(
+                    ["public_profile","email"], function(response){
+                        checkRegister();
+                    },function(failure){
+                        alert("Debes autorizar la aplicacion para acceder.");
+                    });
+                }
+            }, function(failure){
+              alert("Hubo un error conectando a Facebook, intente nuevamente.");
             });
-          };
-
-          (function(d, s, id){
-             var js, fjs = d.getElementsByTagName(s)[0];
-             if (d.getElementById(id)) {return;}
-             js = d.createElement(s); js.id = id;
-             js.src = "http://connect.facebook.net/en_US/sdk.js";
-             fjs.parentNode.insertBefore(js, fjs);
-           }(document, 'script', 'facebook-jssdk'));
+              
+          });
 
         }
     }
