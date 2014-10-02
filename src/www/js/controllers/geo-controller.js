2c18e29fc8ecb486774c55232c98a2f917363143
pdbenitez <pdanibenitez91@gmail.com>
2014-10-02 20:04:52 -0300

    Se sube notificacion de cercania

--- src/www/js/controllers/geo-controller.js
+++ src/www/js/controllers/geo-controller.js
@@ -1,23 +1,27 @@
 angular.module('EPA.controllers')
 
 .controller(
-		'GeolocalizationCtrl',['$scope','superCercanos',
-			function($scope, superCercanos) {
-				navigator.geolocation.getCurrentPosition(function(success) {
-					var position = success.coords;
-					$scope.latitud = position.latitude;
-					$scope.longitud = position.longitude;
-					$scope.$apply();
+		'GeolocalizationCtrl',
+		[
+				'$scope',
+				'superCercanos',
+				function($scope, superCercanos) {
+					navigator.geolocation.getCurrentPosition(function(success) {
+						var position = success.coords;
+						$scope.latitud = position.latitude;
+						$scope.longitud = position.longitude;
+						$scope.$apply();
 
-				}, function(error) {
-					alert('error ' + error);
-
-			});
-				$scope.superCercanos = function() {
-					superCercanos.notificar_cercania(this.latitud,
-							this.longitud).then(function(response) {
-						$scope.mostrarDetalle = true;
 					}, function(error) {
+						alert('error ' + error);
+
 					});
-				};
-			}]);
+					$scope.superCercanos = function() {
+						superCercanos.notificar_cercania(this.latitud,
+								this.longitud).then(function(response) {
+									$scope.supermercados=response.data.data,
+							$scope.mostrarDetalle = true;
+						}, function(error) {
+						});
+					};
+				} ]);
