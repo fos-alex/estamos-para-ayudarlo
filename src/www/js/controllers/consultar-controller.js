a69048af07496ac2377bef4c48d0bda066c564e9
Juliana Bichman <juliana.bichman@gmail.com>
2014-10-04 17:36:21 -0300

    cambios varios

--- src/www/js/controllers/consultar-controller.js
+++ src/www/js/controllers/consultar-controller.js
@@ -3,13 +3,12 @@ angular.module('EPA.controllers')
 .controller('ConsultarCtrl', [
             '$scope', '$stateParams', '$location', 'QRReader',
     function($scope, $stateParams, $location, QRReader) {
-
-    $scope.scan = function () {
-        QRReader.read(function (err, response) {
-            $scope.producto = response[0];
-            return $location.path('/app/consultar/' + $scope.producto.id);
-        });
-    };
+ //   $scope.scan = function () {
+ //       QRReader.read(function (err, response) {
+ //           $scope.producto = response[0];
+ //           return $location.path('/app/consultar/' + $scope.producto.id);
+ //       });
+ //   };
 
 //
 
