102a50ce7da5ded883ecd421081cb8319bd1aca4
Pablo Mussari <pablomussari@gmail.com>
2014-10-09 02:19:06 -0300

    Modo Audio, tts y stt. Faltaria integrarlo con el recorrido

--- src/www/js/controllers/ajustes-controller.js
+++ src/www/js/controllers/ajustes-controller.js
@@ -1,6 +1,36 @@
 angular.module('EPA.controllers')
 
-.controller('AjustesCtrl', ['$scope', function($scope) {
+.controller('AjustesCtrl', ['$scope','$ionicLoading', function($scope,$ionicLoading) {
+
+      
+            $scope.detectarVoz = function() {
+              var maxMatches = 5;
+              var language = "es-AR";
+              $ionicLoading.show({
+                template: 'Espere el tono y hable...'
+              }); 
+              window.plugins.speechrecognizer.start(resultCallback, errorCallback, maxMatches, language);
+            };
+
+            function resultCallback (result){
+                $ionicLoading.hide();  
+                console.log(result);
+                alert(result.results[0][0].transcript);
+            }
+
+            function errorCallback(error){
+                $ionicLoading.hide();  
+                console.log(error);
+            }
+
+            $scope.reproducirVoz = function() {
+                navigator.tts.startup(function(){
+                    var language = "es-AR";
+                    navigator.tts.setLanguage(language, function(){
+                        navigator.tts.speak("Hola, que tal", function(){},function(){});
+                    },function(error){alert("ERROR SETLANGUAGE "+error)});
+                },function(error){alert("ERROR STARTUP "+error)});
+            };
 }])
 
 .controller('GestorDeMapasCtrl', ['$scope', function($scope) {
