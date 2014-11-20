c572598747fd2bb85c85554a69da0d4596fa5cec
Pablo Mussari <pablomussari@gmail.com>
2014-11-20 14:16:10 -0300

    Estadisticas cargando...

--- src/www/js/controllers/estadisticas-controller.js
+++ src/www/js/controllers/estadisticas-controller.js
@@ -1,33 +1,43 @@
 angular.module('EPA.controllers')
 
-    .controller('estadisticaCtrl', ['$scope','$state','estadisticas',
-        function($scope, $state,estadisticas) {
+    .controller('estadisticaCtrl', ['$scope','$state','$ionicLoading','estadisticas',
+        function($scope, $state,$ionicLoading,estadisticas) {
             $scope.mes_estadistica = "09";
             $scope.mostrarTotal=false;
             $scope.mostrarDetalle=false;
 
             $scope.getMes = function(){
+                $ionicLoading.show({
+                    template: 'Consultando datos...'
+                });  
                 estadisticas.getMes(this.mes_estadistica).then(
                     function(response){
                         $scope.mostrarDetalle=true;
                         $scope.mostrarTotal=false;
                         $scope.estadisticas = response.data.data;
+                        $ionicLoading.hide();
                     },
                     function(error){
-
+                        $ionicLoading.hide();
                     }
                 );
 
             };
-                $scope.getTotal = function(){
+            
+            $scope.getTotal = function(){
+                $ionicLoading.show({
+                    template: 'Consultando datos...'
+                });  
                 estadisticas.getTotal(this.mes_estadistica).then(
                      function(response){
                          $scope.mostrarTotal=true;
                          $scope.mostrarDetalle=false;
                          $scope.info= 'La cantidad de compras realizadas fue de: '+ response.data.data.cantidad_compras
                          $scope.info_2= 'El total consumido fue de: $ '+ parseInt(response.data.data.total_por_mes);
+                         $ionicLoading.hide();
                      },
                      function(error){
+                        $ionicLoading.hide();
                      }
                 );
 
